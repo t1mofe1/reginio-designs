3feafc3c9078f24742f5b77a3f58477888cd0577
Timothy Myagi <timofei.magi@gmail.com>
2021-07-06 21:08:11 +0300

    2nd

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(require('compression')());
 app.use(require('cors')());
 app.use(require('helmet')());
 
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
 	res.sendFile(__dirname + '/index.html');
